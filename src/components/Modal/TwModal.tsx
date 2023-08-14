96cc63a8d470adeae668fee43fc7a0341e6af01e
Damian <damian.ovidiu27@gmail.com>
2023-08-14 11:07:21 +0300

    #66

--- src/components/Modal/TwModal.tsx
+++ src/components/Modal/TwModal.tsx
@@ -30,7 +30,7 @@ export default function MyModal(props: ModalProps) {
         type="button"
         onClick={openModal}
         className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-        Join quest!
+        Launch Quest
       </button>
 
       <Transition appear show={isOpen} as={Fragment}>
