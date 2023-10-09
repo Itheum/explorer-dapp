1947eac13d11ef1e77a09285a30b7c9105fae123
Enciusan <enciulescu.alexandru@gmail.com>
2023-10-09 19:15:05 +0300

    caching method

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,23 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const fs = require("fs");
+
 module.exports = function override(config) {
-  console.log('override');
+  let appVersion = new HtmlWebpackPlugin({
+    title: "Caching",
+  });
+  console.log("override");
+  console.log(appVersion, "HtmlPack");
   let loaders = config.resolve;
   loaders.fallback = {
     fs: false,
-    path: require.resolve('path-browserify'),
-    stream: require.resolve('stream-browserify'),
-    crypto: require.resolve('crypto-browserify')
+    path: require.resolve("path-browserify"),
+    stream: require.resolve("stream-browserify"),
+    crypto: require.resolve("crypto-browserify"),
   };
+
+  config.output.filename = `./static/js/main.${process.env.REACT_APP_VERSION}.js`;
+  config.output.chunkFilename = `./static/js/main.${process.env.REACT_APP_VERSION}.chunk.js`;
 
   return config;
 };
