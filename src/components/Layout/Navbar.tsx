035ec339ee4dba52eb299abd409dd728ece709da
Enciusan <enciulescu.alexandru@gmail.com>
2023-10-09 15:39:14 +0300

    deleted 1 lib and fixed text in navbar

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -88,7 +88,7 @@ export const Navbar = () => {
                       "block select-none space-y-1 rounded-md p-3 leading-none !no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
                     }>
                     <NavigationMenuLink>
-                      <div className="text-md font-medium leading-none text-muted-foreground">{item.appName}</div>
+                      <div className="text-md font-medium leading-none text-foreground">{item.appName}</div>
                       <p className="line-clamp-2 text-sm leading-snug text-muted pt-0.5 ">{item?.appDescription}</p>
                     </NavigationMenuLink>
                   </Link>
@@ -120,7 +120,7 @@ export const Navbar = () => {
                       }>
                       <NavigationMenuLink>
                         <div className="text-md font-medium leading-none dark:text-white text-muted-foreground">My Wallet</div>
-                        <p className="line-clamp-2 text-sm leading-snug text-muted pt-0.5">My Wallet Data NFT's</p>
+                        <p className="line-clamp-2 text-sm leading-snug text-muted pt-0.5">View RAW Data</p>
                       </NavigationMenuLink>
                     </Link>
                     <div className="flex flex-col p-3">
