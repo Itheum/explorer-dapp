52770ab9536164416b33bd649c72df9084862bf4
Damian <damian.ovidiu27@gmail.com>
2023-11-02 08:34:41 +0200

    new devnet contracts

--- src/config.tsx
+++ src/config.tsx
@@ -17,41 +17,43 @@ export const dAppName = "Itheum Explorer";
 export const TRAILBLAZER_TOKENS =
   process.env.REACT_APP_ENV_NETWORK && process.env.REACT_APP_ENV_NETWORK === EnvironmentsEnum.devnet
     ? [
-        { tokenIdentifier: "DATANFTFT4-3ba099", nonce: 407 },
-        { tokenIdentifier: "DATANFTFT4-3ba099", nonce: 423 },
-        { tokenIdentifier: "DATANFTFT4-3ba099", nonce: 453 },
+        // { tokenIdentifier: "DATANFTFT-e0b917", nonce: 407 },
+        // { tokenIdentifier: "DATANFTFT-e0b917", nonce: 423 },
+        // { tokenIdentifier: "DATANFTFT-e0b917", nonce: 453 },
       ]
     : [{ tokenIdentifier: "DATANFTFT-e936d4", nonce: 1 }];
 export const MULTIVERSX_BUBBLE_TOKENS =
   process.env.REACT_APP_ENV_NETWORK && process.env.REACT_APP_ENV_NETWORK === EnvironmentsEnum.devnet
     ? [
-        { tokenIdentifier: "DATANFTFT4-3ba099", nonce: 416 },
-        { tokenIdentifier: "DATANFTFT4-3ba099", nonce: 491 },
+        // { tokenIdentifier: "DATANFTFT-e0b917", nonce: 416 },
+        // { tokenIdentifier: "DATANFTFT-e0b917", nonce: 491 },
       ]
     : [{ tokenIdentifier: "DATANFTFT-e936d4", nonce: 2 }];
 export const MULTIVERSX_INFOGRAPHICS_TOKENS =
   process.env.REACT_APP_ENV_NETWORK && process.env.REACT_APP_ENV_NETWORK === EnvironmentsEnum.devnet
     ? [
-        { tokenIdentifier: "DATANFTFT4-3ba099", nonce: 469 },
-        { tokenIdentifier: "DATANFTFT4-3ba099", nonce: 476 },
-        { tokenIdentifier: "DATANFTFT4-3ba099", nonce: 480 },
-        { tokenIdentifier: "DATANFTFT4-3ba099", nonce: 490 },
+        // { tokenIdentifier: "DATANFTFT-e0b917", nonce: 469 },
+        // { tokenIdentifier: "DATANFTFT-e0b917", nonce: 476 },
+        // { tokenIdentifier: "DATANFTFT-e0b917", nonce: 480 },
+        // { tokenIdentifier: "DATANFTFT-e0b917", nonce: 490 },
       ]
     : [{ tokenIdentifier: "DATANFTFT-e936d4", nonce: 3 }];
 export const NF_TUNES_TOKENS =
   process.env.REACT_APP_ENV_NETWORK && process.env.REACT_APP_ENV_NETWORK === EnvironmentsEnum.devnet
     ? [
-        { tokenIdentifier: "DATANFTFT4-3ba099", nonce: 527 },
-        { tokenIdentifier: "DATANFTFT4-3ba099", nonce: 529 },
+        // { tokenIdentifier: "DATANFTFT-e0b917", nonce: 527 },
+        // { tokenIdentifier: "DATANFTFT-e0b917", nonce: 529 },
       ]
     : [{ tokenIdentifier: "DATANFTFT-e936d4", nonce: 4 }];
 
 export const CC_SHOW_SIZE = 10;
 export const PLAYSTATION_GAMER_PASSPORT_TOKENS = [
-  { tokenIdentifier: "DATANFTFT4-3ba099", nonce: 379 },
-  { tokenIdentifier: "DATANFTFT4-3ba099", nonce: 380 },
+  // { tokenIdentifier: "DATANFTFT-e0b917", nonce: 379 },
+  // { tokenIdentifier: "DATANFTFT-e0b917", nonce: 380 },
 ];
-export const ESDT_BUBBLE_TOKENS = [{ tokenIdentifier: "DATANFTFT4-3ba099", nonce: 417 }];
+export const ESDT_BUBBLE_TOKENS = [
+  // { tokenIdentifier: "DATANFTFT-e0b917", nonce: 417 }
+];
 export const EB_SHOW_SIZE = 10;
 
 export const MARKETPLACE_DETAILS_PAGE =
@@ -62,9 +64,7 @@ export const MARKETPLACE_DETAILS_PAGE =
 export const MAINNET_EXPLORER_ADDRESS = "https://explorer.multiversx.com";
 
 export const SUPPORTED_COLLECTIONS =
-  process.env.REACT_APP_ENV_NETWORK && process.env.REACT_APP_ENV_NETWORK === EnvironmentsEnum.devnet
-    ? ["DATANFTFT4-3ba099", "DATALT2-39ee0c"]
-    : ["DATANFTFT-e936d4"];
+  process.env.REACT_APP_ENV_NETWORK && process.env.REACT_APP_ENV_NETWORK === EnvironmentsEnum.devnet ? ["DATANFTFT-e0b917"] : ["DATANFTFT-e936d4"];
 
 export const SUPPORTED_APPS =
   process.env.REACT_APP_ENV_NETWORK && process.env.REACT_APP_ENV_NETWORK === EnvironmentsEnum.devnet
