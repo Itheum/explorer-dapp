ce88ceafd19437f187fae7cbc3ac910ca8f8e6b3
Enciusan <enciulescu.alexandru@gmail.com>
2023-12-15 22:17:29 +0200

    line graph

--- src/pages/AppMarketplace/AshswapPOC/components/TradeVolume.tsx
+++ src/pages/AppMarketplace/AshswapPOC/components/TradeVolume.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import BigNumber from "bignumber.js";
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
+import { Line } from "react-chartjs-2";
 
 type TradeVolumeProps = {
   totalVolume: BigNumber.Value;
@@ -8,6 +10,45 @@ type TradeVolumeProps = {
 
 export const TradeVolume: React.FC<TradeVolumeProps> = (props) => {
   const { totalVolume, volumeHistory } = props;
+
+  ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: "Volume history",
+      },
+    },
+  };
+
+  const takeLineLabels = volumeHistory.map((volumeHistory: any) => {
+    return { volumeHistory: volumeHistory.headerText, volumeAmount: volumeHistory.volume.toNumber() };
+  });
+  // console.log(takeLineLabels);
+
+  const data = {
+    labels: takeLineLabels.map((item: any) => item.volumeHistory),
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: takeLineLabels.map((item: any) => item.volumeAmount),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+        borderDashOffset: 0.0,
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+      },
+    ],
+  };
 
   return (
     <div className="flex flex-col border border-amber-300 p-3 rounded-lg">
@@ -28,6 +69,9 @@ export const TradeVolume: React.FC<TradeVolumeProps> = (props) => {
                 </div>
               );
             })}
+          </div>
+          <div className="w-[100%]">
+            <Line options={options} data={data} />
           </div>
         </div>
       </div>
