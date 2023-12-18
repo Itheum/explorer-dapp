4b6e57c9fa6c3b013c09b2476d5d2a4b2d641423
Enciusan <enciulescu.alexandru@gmail.com>
2023-12-18 15:40:32 +0200

    fixed the background on hover items

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { DataNft, ViewDataReturnType } from "@itheum/sdk-mx-data-nft/out";
-import { ArrowBigLeft, Loader2, Play, Pause, Library, RefreshCcwDot, Volume2, Volume1, VolumeX, SkipBack, SkipForward } from "lucide-react";
+import { ArrowBigLeft, Library, Loader2, Pause, Play, RefreshCcwDot, SkipBack, SkipForward, Volume1, Volume2, VolumeX } from "lucide-react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -250,159 +250,167 @@ export const AudioPlayer = (props: AudioPlayerProps) => {
   };
 
   return (
-    <div className="p-2 md:p-12 relative overflow-hidden">
-      {displayPlaylist ? (
-        <div className="w-full h-[500px] ">
-          <button
-            className="border-[1px] border-foreground/40 select-none flex flex-col items-center justify-center md:flex-row bg-[#fafafa]/50 dark:bg-[#0f0f0f]/25  p-2 gap-2 text-xs relative cursor-pointer  transition-shadow duration-300 shadow-xl hover:shadow-inner hover:shadow-sky-200 dark:hover:shadow-teal-200   bg-[#27293d] rounded-2xl overflow-hidden   "
-            onClick={() => setDisplayPlaylist(false)}>
-            <ArrowBigLeft />
-          </button>
-          <div className="grid grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 mx-4  mt-6 mb-20">
-            {props.songs.map((song: any, index: number) => {
-              return (
-                <div
-                  key={index}
-                  onClick={() => {
-                    setCurrentTrackIndex(index);
-                    setDisplayPlaylist(false);
-                  }}
-                  className={`border-[1px] border-foreground/40 select-none flex flex-col items-center justify-center md:flex-row bg-[#fafafa]/50 dark:bg-[#0f0f0f]/25  p-2 gap-2 text-xs relative cursor-pointer  transition-shadow duration-300 shadow-xl hover:shadow-inner hover:shadow-sky-200 dark:hover:shadow-teal-200   bg-[#27293d] rounded-2xl overflow-hidden text-foreground`}>
-                  <div className="w-[80%] md:w-[60%] h-32 flex items-center justify-center">
-                    <img
-                      src={song.cover_art_url}
-                      alt={"Not Loaded"}
-                      className={`flex items-center justify-center w-24 h-24 rounded-md border border-grey-900 `}
-                      onError={({ currentTarget }) => {
-                        currentTarget.src = theme === "light" ? DEFAULT_SONG_LIGHT_IMAGE : DEFAULT_SONG_IMAGE;
-                      }}
-                    />
-                  </div>
-
-                  <div className="w-8/12 flex flex-col items-center justify-center">
-                    <h6 className=" truncate text-base text-foreground">{song.title}</h6>
-
-                    <p className="truncate text-sm text-center text-foreground">{song.artist}</p>
-                    <p className="text-xs text-center text-muted-foreground">{song.album}</p>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      ) : (
-        <div className="overflow-hidden  w-full flex flex-col bg-bgWhite dark:bg-bgDark items-center justify-center">
-          <div className=" select-none h-[30%] bg-[#FaFaFa]/25 dark:bg-[#0F0F0F]/25   border-[1px] border-foreground/40  relative md:w-[60%] flex flex-col rounded-xl">
-            <div className="px-10 pt-10 pb-4 flex items-center">
-              <img
-                src={props.songs[currentTrackIndex]?.cover_art_url}
-                alt="Album Cover"
-                className=" select-none w-24 h-24 rounded-md mr-6 border border-grey-900"
-                onError={({ currentTarget }) => {
-                  currentTarget.src = theme === "light" ? DEFAULT_SONG_LIGHT_IMAGE : DEFAULT_SONG_IMAGE;
-                }}
-              />
-
-              <div className="flex flex-col select-text">
-                <div>
-                  <span className="font-sans text-lg font-medium leading-7 text-foreground">{props.songs[currentTrackIndex]?.title}</span>{" "}
-                  <span className="ml-2 font-sans text-base font-medium text-muted-foreground">{props.songs[currentTrackIndex]?.date.split("T")[0]}</span>
-                </div>
-
-                <span className="font-sans text-base font-medium text-foreground/60">{props.songs[currentTrackIndex]?.category}</span>
-                <span className="font-sans text-lg font-medium leading-6 text-foreground">{props.songs[currentTrackIndex]?.artist}</span>
-                <span className="font-sans text-base font-medium leading-6 text-muted-foreground">{props.songs[currentTrackIndex]?.album}</span>
-              </div>
-            </div>
-
-            <div className="gap-2 text-foreground select-none w-full flex flex-row justify-center items-center px-10 pb-6 ">
-              <span className="w-[4rem] p-2 text-xs font-sans font-medium text-muted-foreground ">{currentTime}</span>
-              <input
-                type="range"
-                min="0"
-                max="100"
-                step="0.01"
-                value={progress}
-                onChange={(e) => handleProgressChange(Number(e.target.value))}
-                className="accent-black dark:accent-white w-full bg-white mx-auto  focus:outline-none cursor-pointer"
-              />{" "}
-              <span className="w-[4rem] p-2 text-xs font-sans font-medium text-muted-foreground ">{duration}</span>
-            </div>
-
-            <div className="select-none p-2 bg-[#0f0f0f]/10 dark:bg-[#0F0F0F]/50 rounded-b-xl   border-t border-gray-400 dark:border-gray-900  flex items-center justify-between z-10 ">
-              <div className="ml-2 xl:pl-8 flex w-[20%]">
-                {volume === 0 ? <VolumeX /> : volume >= 0.5 ? <Volume2 /> : <Volume1 />}
-                <input
-                  type="range"
-                  min="0"
-                  max="1"
-                  step="0.01"
-                  value={volume}
-                  onChange={(e) => handleVolumeChange(Number(e.target.value))}
-                  className="accent-black dark:accent-white  w-[70%] cursor-pointer ml-2 "></input>
-              </div>
-              <button className="cursor-pointer" onClick={handlePrevButton}>
-                <SkipBack />
+    <div className="bg-gradient-to-br from-[#00C79740] to-[#3D00EA20] bg-blend-multiply">
+      <div className="bg-[#1b1b1b10] backdrop-contrast-[1.10]">
+        <div className="p-2 md:p-12 relative overflow-hidden">
+          {displayPlaylist ? (
+            <div className="w-full h-[500px] ">
+              <button
+                className="border-[1px] border-foreground/40 select-none flex flex-col items-center justify-center md:flex-row bg-[#fafafa]/50 dark:bg-[#0f0f0f]/25 p-2 gap-2 text-xs relative cursor-pointer transition-shadow duration-300 shadow-xl hover:shadow-inner hover:shadow-sky-200 dark:hover:shadow-teal-200 bg-[#27293d] rounded-2xl overflow-hidden   "
+                onClick={() => setDisplayPlaylist(false)}>
+                <ArrowBigLeft />
               </button>
-              <div className="w-16 h-16 rounded-full bg-slate-100 dark:bg-slate-900/0 border border-grey-300 shadow-xl flex items-center justify-center">
-                <button onClick={togglePlay} className="focus:outline-none" disabled={!isLoaded}>
-                  {!isLoaded ? <Loader2 className="animate-spin " /> : isPlaying ? <Pause /> : <Play className="ml-1" />}
-                </button>
-              </div>
-              <button className="cursor-pointer" onClick={handleNextButton}>
-                <SkipForward />
-              </button>
-              <button className="cursor-pointer" onClick={repeatTrack}>
-                <RefreshCcwDot />
-              </button>
-              <button className="mr-2  xl:pr-8" onClick={showPlaylist}>
-                <Library />
-              </button>
-            </div>
-          </div>
-          <div className="w-[80%] 2xl:w-[70%] mt-8 mx-auto">
-            <h4 className="select-none flex justify-start font-semibold text-foreground mt-4 mb-2">{`Tracklist ${props.songs.length} songs`} </h4>
-            <Slider {...settings}>
-              {props.songs.map((song: any, index: number) => {
-                return (
-                  <div key={index} className=" w-32 xl:w-64 shadow-none flex items-center justify-center">
+              <div className="grid grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 mx-4 mt-6 mb-20">
+                {props.songs.map((song: any, index: number) => {
+                  return (
                     <div
+                      key={index}
                       onClick={() => {
                         setCurrentTrackIndex(index);
+                        setDisplayPlaylist(false);
                       }}
-                      className="mx-auto w-32 xl:w-64 select-none flex flex-col xl:flex-row items-center justify-center bg-[#fafafa]/25 dark:bg-[#0f0f0f]/25 cursor-pointer transition-shadow duration-300 shadow-xl hover:shadow-inner hover:shadow-teal-200 rounded-2xl text-foreground border-[1px] border-foreground/40">
-                      <div className="w-[80%] xl:w-[40%] justify-center">
+                      className={`border-[1px] border-foreground/40 select-none flex flex-col items-center justify-center md:flex-row bg-[#fafafa]/50 dark:bg-[#0f0f0f]/25 p-2 gap-2 text-xs cursor-pointer duration-300 shadow-xl hover:shadow-inner hover:shadow-sky-200 dark:hover:shadow-teal-200 rounded-2xl overflow-hidden text-foreground`}>
+                      <div className="w-[80%] md:w-[60%] h-32 flex items-center justify-center">
                         <img
                           src={song.cover_art_url}
-                          alt="Album Cover"
-                          className="h-24 p-2 rounded-md"
+                          alt={"Not Loaded"}
+                          className={`flex items-center justify-center w-24 h-24 rounded-md border border-grey-900 `}
                           onError={({ currentTarget }) => {
                             currentTarget.src = theme === "light" ? DEFAULT_SONG_LIGHT_IMAGE : DEFAULT_SONG_IMAGE;
                           }}
                         />
                       </div>
-                      <div className=" xl:w-[60%] flex flex-col justify-center text-center  ">
-                        <h6 className=" text-base text-foreground truncate ">{song.title}</h6>
-                        <p className="font-sans text-base font-medium leading-6 text-muted-foreground truncate">{song.artist}</p>
+
+                      <div className="w-8/12 flex flex-col items-center justify-center">
+                        <h6 className=" truncate text-base text-foreground">{song.title}</h6>
+
+                        <p className="truncate text-sm text-center text-foreground">{song.artist}</p>
+                        <p className="text-xs text-center text-muted-foreground">{song.album}</p>
                       </div>
                     </div>
+                  );
+                })}
+              </div>
+            </div>
+          ) : (
+            <div className="overflow-hidden  w-full flex flex-col bg-bgWhite dark:bg-bgDark items-center justify-center">
+              <div className=" select-none h-[30%] bg-[#FaFaFa]/25 dark:bg-[#0F0F0F]/25   border-[1px] border-foreground/40  relative md:w-[60%] flex flex-col rounded-xl">
+                <div className="px-10 pt-10 pb-4 flex items-center">
+                  <img
+                    src={props.songs[currentTrackIndex]?.cover_art_url}
+                    alt="Album Cover"
+                    className=" select-none w-24 h-24 rounded-md mr-6 border border-grey-900"
+                    onError={({ currentTarget }) => {
+                      currentTarget.src = theme === "light" ? DEFAULT_SONG_LIGHT_IMAGE : DEFAULT_SONG_IMAGE;
+                    }}
+                  />
+
+                  <div className="flex flex-col select-text">
+                    <div>
+                      <span className="font-sans text-lg font-medium leading-7 text-foreground">{props.songs[currentTrackIndex]?.title}</span>{" "}
+                      <span className="ml-2 font-sans text-base font-medium text-muted-foreground">{props.songs[currentTrackIndex]?.date.split("T")[0]}</span>
+                    </div>
+
+                    <span className="font-sans text-base font-medium text-foreground/60">{props.songs[currentTrackIndex]?.category}</span>
+                    <span className="font-sans text-lg font-medium leading-6 text-foreground">{props.songs[currentTrackIndex]?.artist}</span>
+                    <span className="font-sans text-base font-medium leading-6 text-muted-foreground">{props.songs[currentTrackIndex]?.album}</span>
                   </div>
-                );
-              })}
-            </Slider>
-            <style>
-              {`
+                </div>
+
+                <div className="gap-2 text-foreground select-none w-full flex flex-row justify-center items-center px-10 pb-6">
+                  <span className="w-[4rem] p-2 text-xs font-sans font-medium text-muted-foreground">{currentTime}</span>
+                  <input
+                    type="range"
+                    min="0"
+                    max="100"
+                    step="0.01"
+                    value={progress}
+                    onChange={(e) => handleProgressChange(Number(e.target.value))}
+                    className="accent-black dark:accent-white w-full bg-white mx-auto  focus:outline-none cursor-pointer"
+                  />{" "}
+                  <span className="w-[4rem] p-2 text-xs font-sans font-medium text-muted-foreground ">{duration}</span>
+                </div>
+
+                <div className="select-none p-2 bg-[#0f0f0f]/10 dark:bg-[#0F0F0F]/50 rounded-b-xl border-t border-gray-400 dark:border-gray-900  flex items-center justify-between z-10 ">
+                  <div className="ml-2 xl:pl-8 flex w-[20%]">
+                    {volume === 0 ? <VolumeX /> : volume >= 0.5 ? <Volume2 /> : <Volume1 />}
+                    <input
+                      type="range"
+                      min="0"
+                      max="1"
+                      step="0.01"
+                      value={volume}
+                      onChange={(e) => handleVolumeChange(Number(e.target.value))}
+                      className="accent-black dark:accent-white w-[70%] cursor-pointer ml-2 "></input>
+                  </div>
+                  <button className="cursor-pointer" onClick={handlePrevButton}>
+                    <SkipBack className="w-full hover:scale-105" />
+                  </button>
+                  <div className="w-16 h-16 rounded-full bg-slate-100 dark:bg-slate-900/0 border border-grey-300 shadow-xl flex items-center justify-center">
+                    <button onClick={togglePlay} className="focus:outline-none" disabled={!isLoaded}>
+                      {!isLoaded ? (
+                        <Loader2 className="w-full text-center animate-spin  hover:scale-105" />
+                      ) : isPlaying ? (
+                        <Pause className="w-full text-center hover:scale-105" />
+                      ) : (
+                        <Play className="w-full text-center hover:scale-105" />
+                      )}
+                    </button>
+                  </div>
+                  <button className="cursor-pointer" onClick={handleNextButton}>
+                    <SkipForward className="w-full hover:scale-105" />
+                  </button>
+                  <button className="cursor-pointer" onClick={repeatTrack}>
+                    <RefreshCcwDot className="w-full hover:scale-105" />
+                  </button>
+                  <button className="mr-2  xl:pr-8" onClick={showPlaylist}>
+                    <Library className="w-full hover:scale-105" />
+                  </button>
+                </div>
+              </div>
+              <div className="w-[80%] 2xl:w-[70%] mt-8 mx-auto">
+                <h4 className="select-none flex justify-start font-semibold text-foreground mt-4 mb-2">{`Tracklist ${props.songs.length} songs`} </h4>
+                <Slider {...settings}>
+                  {props.songs.map((song: any, index: number) => {
+                    return (
+                      <div key={index} className=" w-32 xl:w-64 shadow-none flex items-center justify-center">
+                        <div
+                          onClick={() => {
+                            setCurrentTrackIndex(index);
+                          }}
+                          className="mx-auto w-32 xl:w-64 select-none flex flex-col xl:flex-row items-center justify-center bg-[#fafafa]/25 dark:bg-[#0f0f0f]/25 cursor-pointer transition-shadow duration-300 shadow-xl hover:shadow-inner hover:shadow-teal-200 rounded-2xl text-foreground border-[1px] border-foreground/40">
+                          <div className="w-[80%] xl:w-[40%] justify-center">
+                            <img
+                              src={song.cover_art_url}
+                              alt="Album Cover"
+                              className="h-24 p-2 rounded-md"
+                              onError={({ currentTarget }) => {
+                                currentTarget.src = theme === "light" ? DEFAULT_SONG_LIGHT_IMAGE : DEFAULT_SONG_IMAGE;
+                              }}
+                            />
+                          </div>
+                          <div className=" xl:w-[60%] flex flex-col justify-center text-center  ">
+                            <h6 className=" text-base text-foreground truncate ">{song.title}</h6>
+                            <p className="font-sans text-base font-medium leading-6 text-muted-foreground truncate">{song.artist}</p>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </Slider>
+                <style>
+                  {`
                 /* CSS styles for Swiper navigation arrows  */
                 .slick-prev:before,
                 .slick-next:before {
                 color: ${theme === "light" ? "black;" : "white;"},
                     }`}
-            </style>
-          </div>
+                </style>
+              </div>
+            </div>
+          )}
         </div>
-      )}
-      <div className="z-[-1] ml-[-10%]  mt-[-25%] h-[50%] w-[60%] opacity-75 blur-[300px] absolute bg-[#00C797] rounded-full"></div>
-      <div className="z-[-1] mt-[-15%] ml-[40%] h-[50%] w-[60%] opacity-75 blur-[300px] absolute bg-[#3D00EA] rounded-full "></div>
+      </div>
     </div>
   );
 };
