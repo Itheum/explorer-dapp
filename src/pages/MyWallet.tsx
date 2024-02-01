3374a01a76bbfb2a4c4269f8e4aeca26f240c741
Damian <damian.ovidiu27@gmail.com>
2024-02-01 08:44:16 +0200

    feat: video nfts in wallet

--- src/pages/MyWallet.tsx
+++ src/pages/MyWallet.tsx
@@ -109,6 +109,11 @@ export const MyWallet = () => {
         setIsAutoOpenFormat(false);
         res.data = DOMPurify.sanitize(await (res.data as Blob).text());
         setIsDomPurified(true);
+      } else if (res.contentType.search("video/mp4") >= 0) {
+        setIsAutoOpenFormat(false);
+        const videoObject = window.URL.createObjectURL(new Blob([res.data], { type: res.contentType }));
+        res.data = videoObject;
+        blobDataType = BlobDataType.VIDEO;
       } else {
         setIsAutoOpenFormat(false);
         // we don't support that format
@@ -177,6 +182,10 @@ export const MyWallet = () => {
                     </div>
                   ) : viewDataRes.blobDataType === BlobDataType.SVG ? (
                     <SVG src={viewDataRes.data} style={{ width: "100%", height: "auto" }} />
+                  ) : viewDataRes.blobDataType === BlobDataType.VIDEO ? (
+                    <video className="w-auto h-auto mx-auto my-4" style={{ maxHeight: "600px" }} controls>
+                      <source src={viewDataRes.data} type="video/mp4"></source>
+                    </video>
                   ) : (
                     <div className="p-2">
                       {(isAutoOpenFormat && (
