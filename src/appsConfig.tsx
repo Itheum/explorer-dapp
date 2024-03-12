129615499b2701eb4c0aa60d714b279f5c9d0737
Damian <damian.ovidiu27@gmail.com>
2024-03-12 12:50:22 +0200

    chore: add DATANFTFT-e0b917-8a to nf-tunes

--- src/appsConfig.tsx
+++ src/appsConfig.tsx
@@ -44,6 +44,7 @@ export const NF_TUNES_TOKENS: app_token[] = IS_DEVNET
       { tokenIdentifier: "DATANFTFT-e0b917", nonce: 48 },
       { tokenIdentifier: "DATANFTFT-e0b917", nonce: 131 },
       { tokenIdentifier: "DATANFTFT-e0b917", nonce: 134 },
+      { tokenIdentifier: "DATANFTFT-e0b917", nonce: 138 },
       // { tokenIdentifier: "DATANFTFT-e0b917", nonce: 529 },
     ]
   : [{ tokenIdentifier: "DATANFTFT-e936d4", nonce: 4 }];
