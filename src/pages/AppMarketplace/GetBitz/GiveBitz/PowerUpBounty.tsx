1108413ab04b5d27c48c1d22f372b316ef7a0004
Benedek Robert George <benedek_robert2001@yahoo.com>
2024-05-16 11:00:47 +0300

    feat: add a finalized flag and UI to the finalized data bounties

--- src/pages/AppMarketplace/GetBitz/GiveBitz/PowerUpBounty.tsx
+++ src/pages/AppMarketplace/GetBitz/GiveBitz/PowerUpBounty.tsx
@@ -3,40 +3,25 @@ import { useGetNetworkConfig } from "@multiversx/sdk-dapp/hooks";
 import { ExternalLinkIcon } from "lucide-react";
 import moment from "moment-timezone";
 import { Link } from "react-router-dom";
+import stampFinalized from "assets/img/getbitz/givebitz/stampFinalized.png";
 import { MXAddressLink } from "components";
+import { MARKETPLACE_DETAILS_PAGE } from "config";
 import { useGetAccount } from "hooks";
 import GiveBitzLowerCard from "./GiveBitzLowerCard";
+import { GiveBitzDataBounty } from "../config";
+import { cn } from "libs/utils";
 
 type PowerUpBountyProps = {
-  bountySubmitter: string;
-  bountyId: string;
-  title: string;
-  summary: string;
-  readMoreLink: string;
-  submittedOnTs: number;
-  fillPerks: string;
-  receivedBitzSum?: number;
-  giverCounts?: number;
+  bounty: GiveBitzDataBounty;
   sendPowerUp: any;
   fetchGivenBitsForGetter: any;
   fetchGetterLeaderBoard: any;
 };
 
 const PowerUpBounty = (props: PowerUpBountyProps) => {
-  const {
-    bountySubmitter,
-    bountyId,
-    title,
-    summary,
-    readMoreLink,
-    submittedOnTs,
-    fillPerks,
-    giverCounts,
-    receivedBitzSum,
-    sendPowerUp,
-    fetchGivenBitsForGetter,
-    fetchGetterLeaderBoard,
-  } = props;
+  const { bounty, sendPowerUp, fetchGivenBitsForGetter, fetchGetterLeaderBoard } = props;
+  const { bountySubmitter, bountyId, title, summary, readMoreLink, submittedOnTs, fillPerks, giverCounts, receivedBitzSum, finalizedDataNftIdentifier } =
+    bounty;
   const {
     network: { explorerAddress },
   } = useGetNetworkConfig();
@@ -45,7 +30,11 @@ const PowerUpBounty = (props: PowerUpBountyProps) => {
   return (
     <div className="power-up-tile border  min-w-[260px] max-w-[360px] relative rounded-3xl">
       <div className="group" data-highlighter>
-        <div className="relative bg-[#35d9fa]/80 dark:bg-[#35d9fa]/30  rounded-3xl p-[2px] before:absolute before:w-96 before:h-96 before:-left-48 before:-top-48 before:bg-[#35d9fa]  before:rounded-full before:opacity-0 before:pointer-events-none before:transition-opacity before:duration-500 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:hover:opacity-20 before:z-30 before:blur-[100px] after:absolute after:inset-0 after:rounded-[inherit] after:opacity-0 after:transition-opacity after:duration-500 after:[background:_radial-gradient(250px_circle_at_var(--mouse-x)_var(--mouse-y),theme(colors.sky.400),transparent)] after:group-hover:opacity-100 after:z-10 overflow-hidden">
+        <div
+          className={cn(
+            "relative bg-[#35d9fa]/80 dark:bg-[#35d9fa]/60  rounded-3xl p-[2px] before:absolute before:w-96 before:h-96 before:-left-48 before:-top-48 before:bg-[#35d9fa]  before:rounded-full before:opacity-0 before:pointer-events-none before:transition-opacity before:duration-500 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:hover:opacity-20 before:z-30 before:blur-[100px] after:absolute after:inset-0 after:rounded-[inherit] after:opacity-0 after:transition-opacity after:duration-500 after:[background:_radial-gradient(250px_circle_at_var(--mouse-x)_var(--mouse-y),theme(colors.sky.400),transparent)] after:group-hover:opacity-100 after:z-10 overflow-hidden",
+            finalizedDataNftIdentifier ? " bg-[#35d9fa]/40   dark:bg-[#35d9fa]/20" : ""
+          )}>
           <div className="relative h-full bg-neutral-950/40 dark:bg-neutral-950/60  rounded-[inherit] z-20 overflow-hidden p-4 md:p-8">
             <div className="flex justify-between items-center text-sm md:text-base">
               <div className=" bg-[#2495AC] dark:bg-[#022629] p-1 px-3 rounded-2xl shadow-inner shadow-[#35d9fa]/30  ">
@@ -89,23 +78,43 @@ const PowerUpBounty = (props: PowerUpBountyProps) => {
                   })}
                 </ul>
               </div>
-              {address && (
-                <GiveBitzLowerCard
-                  bountySubmitter={bountySubmitter}
-                  bountyId={bountyId}
-                  sendPowerUp={sendPowerUp}
-                  fetchGivenBitsForGetter={fetchGivenBitsForGetter}
-                  fetchGetterLeaderBoard={fetchGetterLeaderBoard}
-                />
+              {address && finalizedDataNftIdentifier ? (
+                <div className="h-[21rem]">
+                  <img src={stampFinalized} alt="Finalized" className="w-40 mx-auto" />
+                  <div className="text-center text-2xl bg-[#2495AC] dark:bg-[#022629] p-1 px-3 rounded-2xl shadow-inner shadow-[#35d9fa]/30">Finalized</div>
+                  <Link
+                    to={MARKETPLACE_DETAILS_PAGE + finalizedDataNftIdentifier}
+                    target="_blank"
+                    className="relative z-[100] mt-16 text-[#35d9fa] hover:underline   md:text-xl  flex flex-row gap-1 justify-center items-center">
+                    View Collection
+                    <ExternalLinkIcon width={20} />
+                  </Link>
+                </div>
+              ) : (
+                <>
+                  {address && (
+                    <GiveBitzLowerCard
+                      bountySubmitter={bountySubmitter}
+                      bountyId={bountyId}
+                      sendPowerUp={sendPowerUp}
+                      fetchGivenBitsForGetter={fetchGivenBitsForGetter}
+                      fetchGetterLeaderBoard={fetchGetterLeaderBoard}
+                    />
+                  )}
+                </>
               )}
 
-              <Link
-                to="https://docs.google.com/forms/d/e/1FAIpQLSctQIpxSw-TnJzP52nUddJEun28DUcObqbUGH8ulHEd0MNmaQ/viewform?usp=sf_link"
-                target="_blank"
-                className="relative z-[100] mt-2 text-[#35d9fa] hover:underline text-xs md:text-sm  flex flex-row gap-1 justify-center items-center">
-                Fill this bounty as a Data NFT!
-                <ExternalLinkIcon width={12} />
-              </Link>
+              {!finalizedDataNftIdentifier ? (
+                <Link
+                  to="https://docs.google.com/forms/d/e/1FAIpQLSctQIpxSw-TnJzP52nUddJEun28DUcObqbUGH8ulHEd0MNmaQ/viewform?usp=sf_link"
+                  target="_blank"
+                  className="relative z-[100] mt-2 text-[#35d9fa] hover:underline text-xs md:text-sm  flex flex-row gap-1 justify-center items-center">
+                  Fill this bounty as a Data NFT!
+                  <ExternalLinkIcon width={12} />
+                </Link>
+              ) : (
+                <div className="h-6"></div>
+              )}
             </>
           </div>
         </div>
