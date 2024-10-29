30a557bc04537d09948eab2ab7fb9f1304281df7
Mark Paul <paul.mark@gmail.com>
2024-10-29 22:26:59 +1100

    chore: new time capsule nonce

--- src/appsConfig.tsx
+++ src/appsConfig.tsx
@@ -78,11 +78,12 @@ export const TIMECAPSULE_TOKENS: app_token[] = IS_DEVNET
       { tokenIdentifier: "DATANFTFT-e0b917", nonce: 351 },
       { tokenIdentifier: "DATANFTFT-e0b917", nonce: 624 },
       { tokenIdentifier: "DATANFTFT-e0b917", nonce: 625 },
+      { tokenIdentifier: "DATANFTFT-e0b917", nonce: 629 },
     ]
   : [{ tokenIdentifier: "DATANFTFT-e936d4", nonce: 5 }];
 
 export const TIMECAPSULE_XDAY_TOKENS: app_token[] = IS_DEVNET
-  ? [{ tokenIdentifier: "DATANFTFT-e0b917", nonce: 625 }]
+  ? [{ tokenIdentifier: "DATANFTFT-e0b917", nonce: 629 }]
   : [{ tokenIdentifier: "DATANFTFT-e936d4", nonce: 5 }];
 
 export const BOBER_GAME_ROOM_TOKENS: app_token[] = IS_DEVNET
