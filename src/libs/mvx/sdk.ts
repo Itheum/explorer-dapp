d485bfd32f71facdba219bdeb465dea434b2974d
Mark Paul <paul.mark@gmail.com>
2024-11-11 16:06:18 +1100

    fix: remove some unused code for data nft sdk, lastest data nft sdk

--- src/libs/mvx/sdk.ts
+++ src/libs/mvx/sdk.ts
@@ -1,10 +1,8 @@
-import { DataNft, DataNftMarket } from '@itheum/sdk-mx-data-nft';
-import {
-  apiTimeout,
-  ELROND_NETWORK,
-} from 'config';
+// import { DataNft, DataNftMarket } from '@itheum/sdk-mx-data-nft';
+import { DataNftMarket } from "@itheum/sdk-mx-data-nft";
+import { apiTimeout, ELROND_NETWORK } from "config";
 
-// set network config of DataNft class
-DataNft.setNetworkConfig(ELROND_NETWORK);
+// // set network config of DataNft class (not sure why this was here -- so removed)
+// DataNft.setNetworkConfig(ELROND_NETWORK);
 
 export const dataNftMarket = new DataNftMarket(ELROND_NETWORK, apiTimeout);
