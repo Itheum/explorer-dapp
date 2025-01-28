b65ede5026241130026252d04090f6c08e1146d1
Mark Paul <paul.mark@gmail.com>
2025-01-29 10:31:32 +1100

    feature: nftunes update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -41,6 +41,24 @@ export const Home = () => {
 
           <div className="flex flex-col md:flex-row flex-wrap p-2 pb-0 rounded-md">
             <div
+              id="featured2"
+              className="flex md:min-w-[25%] md:flex-1 border-[0.5px] border-neutral-500/90 mt-2 min-h-[200px] md:h-[330px] bg-no-repeat bg-cover rounded-3xl mx-2 py-5 md:py-1"
+              style={{
+                "backgroundImage": `url(https://api.itheumcloud.com/app_nftunes/assets/img/Waveborn_Luminex_Lost_And_Wondering.jpeg)`,
+                "backgroundBlendMode": "multiply",
+                "backgroundColor": "#161616a3",
+              }}>
+              <div className="flex flex-col w-[80%] h-[100%] m-auto justify-center items-center">
+                <h1 className="!text-lg !text-white text-center md:!text-3xl mb-5">Waveborn's "Lost And Wondering" is Live!</h1>
+                <Link to={`${routeNames.nftunes}/?artist-profile=waveborn-luminex`} className="text-base hover:!no-underline hover:text-black">
+                  <Button className="!text-black text-sm tracking-tight relative px-[2.35rem] left-2 bottom-1.5 bg-gradient-to-r from-yellow-300 to-orange-500 transition ease-in-out delay-150 duration-300 hover:translate-y-1.5 hover:-translate-x-[8px] hover:scale-100">
+                    Listen & Collect
+                  </Button>
+                </Link>
+              </div>
+            </div>
+
+            <div
               id="featured1"
               className="flex md:min-w-[25%] md:flex-1 border-[0.5px] border-neutral-500/90 mt-2 min-h-[200px] md:h-[330px] bg-no-repeat bg-cover rounded-3xl mx-2 py-5 md:py-1"
               style={{
@@ -51,24 +69,6 @@ export const Home = () => {
               <div className="flex flex-col w-[80%] h-[100%] m-auto justify-center items-center">
                 <h1 className="!text-lg !text-white text-center md:!text-3xl mb-5">Uncomplex's "ISHQNAMA - JOURNEY OF LOVE" is Live!</h1>
                 <Link to={`${routeNames.nftunes}/?artist-profile=uncomplex`} className="text-base hover:!no-underline hover:text-black">
-                  <Button className="!text-black text-sm tracking-tight relative px-[2.35rem] left-2 bottom-1.5 bg-gradient-to-r from-yellow-300 to-orange-500 transition ease-in-out delay-150 duration-300 hover:translate-y-1.5 hover:-translate-x-[8px] hover:scale-100">
-                    Listen & Collect
-                  </Button>
-                </Link>
-              </div>
-            </div>
-
-            <div
-              id="featured2"
-              className="flex md:min-w-[25%] md:flex-1 border-[0.5px] border-neutral-500/90 mt-2 min-h-[200px] md:h-[330px] bg-no-repeat bg-cover rounded-3xl mx-2 py-5 md:py-1"
-              style={{
-                "backgroundImage": `url(https://api.itheumcloud.com/app_nftunes/assets/img/PhysixDudeAnglesOfIdentity.jpg)`,
-                "backgroundBlendMode": "multiply",
-                "backgroundColor": "#161616a3",
-              }}>
-              <div className="flex flex-col w-[80%] h-[100%] m-auto justify-center items-center">
-                <h1 className="!text-lg !text-white text-center md:!text-3xl mb-5">Physixdude's "Angles of Identity" is Live!</h1>
-                <Link to={`${routeNames.nftunes}/?artist-profile=physixdude`} className="text-base hover:!no-underline hover:text-black">
                   <Button className="!text-black text-sm tracking-tight relative px-[2.35rem] left-2 bottom-1.5 bg-gradient-to-r from-yellow-300 to-orange-500 transition ease-in-out delay-150 duration-300 hover:translate-y-1.5 hover:-translate-x-[8px] hover:scale-100">
                     Listen & Collect
                   </Button>
