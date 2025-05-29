2b8e0ee7173eef201581e050a3e762f9ae58af7b
Mark Paul <paul.mark@gmail.com>
2025-05-29 22:47:56 +1000

    fix: for potential race condition in the DappProvider bootup where devnet was showing for mainnet as chainID from useGetNetworkConfig was reporting as devnet

--- src/contexts/mvx/MvxContextProvider.tsx
+++ src/contexts/mvx/MvxContextProvider.tsx
@@ -1,19 +1,28 @@
 import { FC, ReactNode } from "react";
-import { useGetNetworkConfig } from "@multiversx/sdk-dapp/hooks";
+// import { useGetNetworkConfig } from "@multiversx/sdk-dapp/hooks";
 import { NotificationModal } from "@multiversx/sdk-dapp/UI/NotificationModal/NotificationModal";
 import { SignTransactionsModals } from "@multiversx/sdk-dapp/UI/SignTransactionsModals/SignTransactionsModals";
 import { TransactionsToastList } from "@multiversx/sdk-dapp/UI/TransactionsToastList/TransactionsToastList";
 import { DappProvider } from "@multiversx/sdk-dapp/wrappers/DappProvider/DappProvider";
+import { IS_DEVNET } from "appsConfig";
 import { ELROND_NETWORK, apiTimeout, walletConnectV2ProjectId } from "config";
 import { getMvxRpcApi } from "libs/utils";
 
 export const MvxContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const {
-    network: { chainId: chainID },
-  } = useGetNetworkConfig();
+  // const {
+  //   network: { chainId: chainID },
+  // } = useGetNetworkConfig();
+
+  let mvxRpcApiToUse = "";
+
+  if (IS_DEVNET) {
+    mvxRpcApiToUse = `https://${getMvxRpcApi("D")}`;
+  } else {
+    mvxRpcApiToUse = `https://${getMvxRpcApi("1")}`;
+  }
 
   console.log("MvxContextProvider ELROND_NETWORK", ELROND_NETWORK);
-  console.log("MvxContextProvider chainID", chainID);
+  console.log("MvxContextProvider mvxRpcApiToUse", mvxRpcApiToUse);
 
   return (
     <>
@@ -23,7 +32,7 @@ export const MvxContextProvider: FC<{ children: ReactNode }> = ({ children }) =>
           name: "itheumExplorer",
           apiTimeout,
           walletConnectV2ProjectId,
-          apiAddress: `https://${getMvxRpcApi(chainID)}`,
+          apiAddress: mvxRpcApiToUse,
         }}
         dappConfig={{
           shouldUseWebViewProvider: true,
